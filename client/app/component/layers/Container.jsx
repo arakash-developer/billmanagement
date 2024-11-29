a16dcfb609a7d54cac7ef9a6ee06463829a7dfcd
akash100a <arakash968786@gmail.com>
2024-11-29 17:37:47 +0600

    up

--- client/app/component/layers/Container.jsx
+++ client/app/component/layers/Container.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Container = ({ className , children }) => {
   return (
-    <div className={`mx-auto ${className}`}>
+    <div className={`max-w-[1300px] mx-auto ${className}`}>
         {children}
     </div>
   )
