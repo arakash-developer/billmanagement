af91a1857b0a412a4da6befc344c3e9441200e3f
akash100a <arakash968786@gmail.com>
2024-11-30 04:57:53 +0600

    g

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 let UserModel = require("./models/user");
 app.use(cors({
+  origin:"http://localhost:3000",
   methods:["POST","GET"],
   credentials:true,
 }));
