2b24b341a83e493747f2e6645fb379a19e135e80
akash100a <arakash968786@gmail.com>
2024-11-29 17:49:50 +0600

    up

--- client/app/page.jsx
+++ client/app/page.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import CashMemo from './component/CashMemo'
+import Container from './component/layers/Container'
 
 const Home = () => {
   return (
     <div>
+      <Container>
       Home Page
+      </Container>
     </div>
   )
 }
