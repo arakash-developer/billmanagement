4e8cf85ffb08761ada10845d5abc757aafe7c2eb
akash100a <arakash968786@gmail.com>
2024-11-30 18:31:38 +0600

    update

--- client/app/profile/page.jsx
+++ client/app/profile/page.jsx
@@ -1,18 +1,11 @@
 "use client"
 import React from 'react'
 import Container from '../component/layers/Container';
-import axios from 'axios';
+
 
 const page = () => {
   let handlerlogOut = () => {
-    console.log("AKASH");
-    axios.get('https://billmanagement-server.vercel.app/logout')
-      .then(function (response) {
-        console.log(response.data.status);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
+    localStorage.removeItem('token');
   }
   return (
     <div>
