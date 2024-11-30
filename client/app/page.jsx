7a25c8acdbc269de27c8424e83045475a4a56004
akash100a <arakash968786@gmail.com>
2024-12-01 03:04:13 +0600

    kk

--- client/app/page.jsx
+++ client/app/page.jsx
@@ -1,21 +1,21 @@
 "use client"
-import React, { useContext,useLayoutEffect } from 'react'
+import React, { useContext, useLayoutEffect } from 'react'
 import Login from './component/LogIn'
 import { Contex } from '@/app/contexapi/Rights'
 import { useRouter } from 'next/navigation'
 const page = () => {
   const router = useRouter()
-  let {validated,setValidated} = useContext(Contex)
-  useLayoutEffect(()=>{
+  let { validated, setValidated } = useContext(Contex)
+  useLayoutEffect(() => {
     let token = localStorage.getItem('login')
-    if(token){
+    if (token) {
       setValidated(token)
       router.push('/profile')
     }
-  },[])
+  }, [])
   return (
     <div>
-      <Login/>
+      <Login />
     </div>
 
   )
