ca2e8fc4eca5ccc3418dfd1765a22b44f29b0fb7
akash100a <arakash968786@gmail.com>
2024-12-02 01:53:52 +0600

    uppppppp

--- client/app/auth/withAuth.js
+++ client/app/auth/withAuth.js
@@ -1,20 +1,22 @@
 "use client"
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useRouter } from 'next/navigation'
 // import localStorage from 'localstorage-polyfill';
 
 export default function withAuth(Component){
     return function withAuth(props){
         const router = useRouter()
+        const [valid, setValid] = useState(false);
         useEffect(()=>{
             let valid = localStorage.getItem('login');
+            setValid(valid)
             if(!valid){
                 router.push('/')
             }
-            if(!valid){
-                return null;
-            }
         },[])
+        if(!valid){
+            return null;
+        }
         return <Component {...props} />
 
     }
--- client/app/book/page.jsx
+++ client/app/book/page.jsx
@@ -1,8 +1,35 @@
 "use client"
-import React from 'react'
+import React, { useContext, useEffect, useLayoutEffect, useState } from 'react'
 import withAuth from '../auth/withAuth'
+import { useRouter } from 'next/navigation'
+import { Contex } from '../contexapi/Rights'
 
 const page = () => {
+  let [clients, setData] = useState([])
+  const router = useRouter();
+  let { validated, setValidated } = useContext(Contex)
+  useLayoutEffect(() => {
+    let valid = localStorage.getItem('login');
+    if (valid) {
+      setValidated(true)
+    }
+  }, [])
+  useEffect(() => {
+    let token = localStorage.getItem("token")
+    let getdata = async () => {
+      let blobs = await fetch("https://billmanagement-server.vercel.app/clientdata", {
+        headers: {
+          "token": token ? token : "",
+        }
+      })
+      let response = await blobs.json();
+      let clients = response.clientdata;
+      // console.log(clients);
+      clients?.reverse();
+      setData(clients)
+    }
+    getdata()
+  }, [])
   return (
     <div className='bg-red-700'>Book page</div>
   )
