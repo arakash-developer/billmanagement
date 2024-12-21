059ae739b313792277ce7b7d88bd98b7e96eb1e5
akash100a <arakash968786@gmail.com>
2024-12-21 22:39:58 +0600

    upp

--- client/app/layout.js
+++ client/app/layout.js
@@ -1,20 +1,16 @@
-import localFont from "next/font/local";
 import "./globals.css";
 import Navbar from "./component/layers/Navbar";
 import StoreProvider from "./StoreProvider";
+import localFont from 'next/font/local'
+ 
+// Font files can be colocated inside of `app`
+const euclid = localFont({
+  src: './fonts/euclid.ttf',
+  display: 'swap',
+})
 
 
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata = {
   title: "E-Store",
@@ -24,7 +20,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className={`${euclid.className}`}>
         <StoreProvider>
         <Navbar />
         {children}
