b14b273c285532c6deeb90621b2453c4296b3303
akash100a <arakash968786@gmail.com>
2024-12-22 09:25:21 +0600

    kk

--- client/app/verified/notification/page.jsx
+++ client/app/verified/notification/page.jsx
@@ -1,9 +1,9 @@
 "use client"
 import React, { useContext, useEffect, useLayoutEffect, useState } from 'react'
-import Container from '../component/layers/Container'
+import Container from '../../component/layers/Container'
 import { Contex } from '@/app/contexapi/Rights'
 import { useRouter } from 'next/navigation'
-import withAuth from '../auth/withAuth'
+import withAuth from '../../auth/withAuth'
 
 const page = () => {
     let { validated, setValidated } = useContext(Contex)
