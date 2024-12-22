e9901a71b5e3ceed583556912ea25f6f2bf409bc
akash100a <arakash968786@gmail.com>
2024-12-22 08:55:59 +0600

    upp

--- client/app/page.jsx
+++ client/app/page.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Login from './component/LogIn'
+import Link from 'next/link'
 const page = () => {
   return (
-    <div>
-      Home
+    <div className='w-full h-screen flex flex-col items-center justify-center'>
+      <h1>Website HOME</h1>
+      <Link className='py-3 px-5 bg-blue-600 text-white mt-4' href='/verify'>Verify Page</Link>
     </div>
 
   )
