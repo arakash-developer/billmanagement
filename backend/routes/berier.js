a5f4a391dae84d79711b82ab7c812be88884e829
akash100a <arakash968786@gmail.com>
2024-12-23 19:11:15 +0600

    upupup

--- backend/routes/berier.js
+++ backend/routes/berier.js
@@ -1,36 +1,50 @@
 const express = require("express");
 const router = express.Router();
-const jwt = require('jsonwebtoken');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
+const jwt = require("jsonwebtoken");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const CashModel = require("../models/cash");
 
 let isLoggedIn = (req, res, next) => {
-    let token = req.headers.token
+  try {
+    let token = req.headers.token;
     if (token) {
-        if (token === '') {
-            res.json({
-                result: false,
-                messege: "You Must Be Loged In First...Token Is Empty"
-            })
-        } else {
-            let data = jwt.verify(token, 'Akash')
-            req.userdata = data;
-            next()
-        }
-    } else {
+      if (token === "") {
         res.json({
+          result: false,
+          messege: "You Must Be Loged In First...Token Is Empty",
+        });
+      } else {
+        try {
+          let data = jwt.verify(token, "Akash");
+          req.userdata = data;
+          next();
+        } catch (err) {
+          res.json({
             result: false,
-            messege: "You Must Be Loged In First"
-        })
+            messege: "You Must Be Loged in ,, Token Is Invalid",
+          });
+        }
+      }
+    } else {
+      res.json({
+        result: false,
+        messege: "You Must Be Loged In First",
+      });
     }
-}
+  } catch (error) {
+    res.json({
+      result: false,
+      messege: error.message,
+    });
+  }
+};
 
 router.post("/", isLoggedIn, async (req, res) => {
-    res.status(200).json({
-        result: true,
-        messege: "You Are Loged In",
-    })
-})
+  res.status(200).json({
+    result: true,
+    messege: "You Are Loged In",
+  });
+});
 
-module.exports = router
+module.exports = router;
