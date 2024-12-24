ab5b18a64331eb85c267ad885b0736fb93584ee1
Abdul Kadir <abdulkadir112k@gmail.com>
2024-12-24 19:26:33 +0600

    hgfs

--- client/app/verified/component/Dashboard.jsx
+++ client/app/verified/component/Dashboard.jsx
@@ -9,7 +9,7 @@ const Dashboard = () => {
       <SideBar/>
        <div className='mt-10 mr-40 flex w-full justify-between'>
          <div className=''>
-           <Link href="/cash" className='px-20 py-10 bg-orange-50 flex flex-col items-center border rounded-md hover:shadow-2xl transition-all duration-300'>
+           <Link href="/verified/cash" className='px-20 py-10 bg-orange-50 flex flex-col items-center border rounded-md hover:shadow-2xl transition-all duration-300'>
                 <FcMoneyTransfer className='w-40 h-40'/>
                Cash-Memo
            </Link>
