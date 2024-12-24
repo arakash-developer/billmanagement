e82d4b290684dc1d2708d46beaf6dc960f0d911b
Abdul Kadir <abdulkadir112k@gmail.com>
2024-12-24 18:21:18 +0600

    ghfioh

--- client/app/verified/component/Dashboard.jsx
+++ client/app/verified/component/Dashboard.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import SideBar from './SideBar'
+import Link from 'next/link'
 
 const Dashboard = () => {
   return (
-    <div>Varified Dashboard Page...</div>
+    <div className=''>
+      <SideBar/>
+       <div className='ml-44 '>
+          <Link href="/verified/cash" className='text-2xl font-semibold text-blue-600 py-20 px-28 shadow-sm' >Cash-Memo</Link>
+          <Link href="/verified/cash" className='text-2xl font-semibold text-blue-600 py-20 px-28 shadow-sm' >Cash-Memo</Link>
+          <Link href="/verified/cash" className='text-2xl font-semibold text-blue-600 py-20 px-28 shadow-sm' >Cash-Memo</Link>
+       </div>
+    </div>
   )
 }
 
