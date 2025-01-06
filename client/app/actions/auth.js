93630726412c9caf2f1453147324a78420c3544f
akash100a <arakash968786@gmail.com>
2025-01-06 20:44:26 +0600

    upload

--- client/app/actions/auth.js
+++ client/app/actions/auth.js
@@ -3,40 +3,42 @@ import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
 export const login = async (formData) => {
-    let email = formData.get("email");
-    let password = formData.get("password");
-    let data = { email, password };
-    console.log(data);
-    
-    let url = "https://billmanagement-server.vercel.app/login";
-  
-    try {
-      const response = await fetch(url, {
-        method: "POST",
-        credentials: "include", // To include cookies
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-      });
-  
-      if (!response.ok) {
-        throw new Error(`HTTP error! status: ${response.status}`);
-      }
-  
-      const result = await response.json();
-      console.log("Response:", result);
-      if(result.result){
-        redirect("/dashboard")
-      }else{
-        redirect("/login")
-      }
-  
-      // Optionally, return the result if needed
-      return result;
-    } catch (error) {
-      console.error("Error:", error);
-      throw error; // Re-throwing the error for further handling
+  const cookieStore = await cookies();
+  let email = formData.get("email");
+  let password = formData.get("password");
+  let data = { email, password };
+  // console.log(data);
+
+  let url = "https://billmanagement-server.vercel.app/login";
+
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      credentials: "include", // To include cookies
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
     }
-  };
-  
+
+    const result = await response.json();
+    //   console.log("Response:", result);
+    if (result.result) {
+      let token = result.token;
+      cookieStore.set("token", token);
+      redirect("/dashboard");
+    } else {
+      redirect("/login");
+    }
+
+    // Optionally, return the result if needed
+    return result;
+  } catch (error) {
+    console.error("Error:", error);
+    throw error; // Re-throwing the error for further handling
+  }
+};
